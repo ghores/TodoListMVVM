b535578a2c298a3b036d0ea4f9772f4303c8a997
Ghazimoradi <ghazimoradi.amin@gmail.com>
2025-04-18 18:59:03 +0330

    Create menu

--- app/src/main/java/com/example/todoappmvvm/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoappmvvm/fragments/list/ListFragment.kt
@@ -2,9 +2,13 @@ package com.example.todoappmvvm.fragments.list
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.todoappmvvm.R
 import com.example.todoappmvvm.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
@@ -15,5 +19,23 @@ class ListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Set menu
+        setHasOptionsMenu(true)
+        binding.apply {
+            floatingActionButton.setOnClickListener {
+                findNavController().navigate(R.id.action_listFragment_to_addFragment)
+            }
+            listLayout.setOnClickListener {
+                findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
--- app/src/main/java/com/example/todoappmvvm/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/todoappmvvm/fragments/update/UpdateFragment.kt
@@ -2,14 +2,32 @@ package com.example.todoappmvvm.fragments.update
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.todoappmvvm.R
+import com.example.todoappmvvm.databinding.FragmentAddBinding
+import com.example.todoappmvvm.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
+    //Binding
+    private var _binding: FragmentUpdateBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        _binding = FragmentUpdateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Set menu
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 }
