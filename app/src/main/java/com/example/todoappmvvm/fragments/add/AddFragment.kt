14440c2dcaa836f2703392108e59c01737403e85
Ghazimoradi <ghazimoradi.amin@gmail.com>
2025-04-18 17:17:05 +0330

    Create navigation graph

---
+++ app/src/main/java/com/example/todoappmvvm/fragments/add/AddFragment.kt
@@ -0,0 +1,15 @@
+package com.example.todoappmvvm.fragments.add
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.todoappmvvm.R
+
+class AddFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+}
